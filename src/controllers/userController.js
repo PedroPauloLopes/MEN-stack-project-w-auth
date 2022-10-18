fa79d62e65e39f2f56ccb5cebca591dab44c7c49
pedroplvs <pedroplvs@gmail.com>
2022-10-18 13:54:11 -0300

    .

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
 const UserSchema = require("../models/userSchema");
+const bcrypt = require('bcrypt');
+
 
 const getAll = async (req, res) => {
   UserSchema.find(function (err, users) {
@@ -9,6 +11,33 @@ const getAll = async (req, res) => {
   }) 
 };
 
+const createUser = async (req, res) => {
+  //console.log("SENHA ANTES DO HASH", req.body.password)
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+  //console.log("SENHA DEPOIS DO HASH", hashedPassword, "SENHA DO BODY", req.body.password)
+  req.body.password = hashedPassword
+
+  //console.log("COMO ESTÁ O REQ BODY?", req.body.password)
+
+  try {
+    // acessar as informações que vem no body da requisição
+    const newUser = new UserSchema(req.body);
+
+    // criar o novo usuário
+    const savedUser = await newUser.save();
+
+    // enviar uma resposta
+    res.status(201).send({
+      "message": "Usuário criado com sucesso",
+      savedUser
+    })
+  } catch(e) {
+    console.error(e)
+  } 
+};
+
+
 module.exports = {
-  getAll
+  getAll,
+  createUser
 };
